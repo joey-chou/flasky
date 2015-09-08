eb5eb692cfc1bc5a4d7736baced1c1a5400afd4b
joey-chou <joey-chou@outlook.com>
2015-09-08 18:31:05 +0800

    Dynamic routes(2B)

--- hello.py
+++ hello.py
@@ -9,5 +9,9 @@ app=Flask(__name__)
 def index():
     return '<h1>Hello world!</h1>'
 	
-if __name__='__main__':
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello %s!</h1>' % name
+	
+if __name__=='__main__':
     app.run(debug=True)
