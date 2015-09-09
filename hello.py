b505305084c684d7caf4d2252f1d0e9e64bb682b
joey-chou <joey-chou@outlook.com>
2015-09-09 19:16:20 +0800

    template(3A)

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@
 #! --coding: utf8--
 
 from flask import Flask
+from flask import render_template
 from flask.ext.script import Manager
 
 app=Flask(__name__)
@@ -9,11 +10,11 @@ manager=Manager(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello world!</h1>'
+    return render_template('index.html')
 	
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello %s!</h1>' % name
+    return render_template('user.html',name=name)
 	
 if __name__=='__main__':
     manager.run()
