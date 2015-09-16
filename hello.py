9b21a5e6b26a547564a4563ee20154a68aed2504
joey-chou <joey-chou@outlook.com>
2015-09-16 16:24:27 +0800

    batabase

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 #! usr/bin/env python
 #! --coding: utf8--
 
+import os
 from datetime import datetime
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.script import Manager
@@ -9,18 +10,44 @@ from flask.ext.moment import Moment
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask.ext.sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app=Flask(__name__)
 manager=Manager(app)
 bootstrap=Bootstrap(app)
 moment=Moment(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI']=\
+            'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
 
 class NameForm(Form):
     name = StringField('What is your name?',validators=[Required()])
     submit = SubmitField('Submit')
 
-	
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username	
+
+        
 @app.route('/', methods=['GET','POST'])
 def index():
     form = NameForm()
