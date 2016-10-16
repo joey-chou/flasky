c347fe3bdb7ddf3ff767c615b5883aed34400189
joey-chou <joey-chou@outlook.com>
2016-10-16 15:08:34 +0800

    Mail(B)

--- hello.py
+++ hello.py
@@ -13,6 +13,7 @@ from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_mail import Mail, Message
+from threading import Thread
 
 basedir, filename = os.path.split(os.path.realpath(__file__))
 
@@ -66,13 +67,19 @@ def make_shell_context():
     return dict(app=app, db=db, Role=Role, User=User)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
+def send_async_email(app,msg):
+    with app.app_context():
+        mail.send(msg)
+        
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, 
                   sender = app.config['FLASKY_MAIL_SENDER'], recipients = [to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
-        
+    thr = Thread(target=send_async_email, args=[app,msg])
+    thr.start()
+    return thr
+       
 @app.route('/', methods=['GET','POST'])
 def index():
     form = NameForm()
